d530c62d7306a455d36f9090b6f171751dda9430
jamesg19 <48737452+jamesg19@users.noreply.github.com>
2021-12-14 21:43:14 -0500

    Agregando Goto en traduccion

---
+++ traduccion/Goto.ts
@@ -0,0 +1,18 @@
+import { Arbol } from "../table/arbol";
+import { TablaSimbolos } from "../table/tablasimbolos";
+
+export class Goto{
+    id:string;
+    fila:number;
+    columna:number;
+
+
+    constructor(id:string,fila:number,columna:number){
+        this.id=id;
+        this.fila=fila;
+        this.columna=columna;
+    }
+    traducir(e: TablaSimbolos, arbol: Arbol):any{
+        
+    }
+}
